9dc3398c77579518f4b51daef78516912b66a674
Mihret001 <Bizumercy@gmail.com>
2023-09-11 03:47:03 -0700

    added 6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,15 +8,14 @@
 
 int main(void)
 {
-        int x = 0;
-
-        while (x <= 9)
-        {
-                putchar(x + '0');
-                x++;
-        }
-
-        putchar('\n');
-
-        return (0);
+	int x = 0;
+	
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	
+	putchar('\n');
+	return (0);
 }
