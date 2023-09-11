edcca7e0df76f5faacb01d7bfbf4cc720b665161
Mihret001 <Bizumercy@gmail.com>
2023-09-11 08:13:35 -0700

    added 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	while (x < 9)
+	{
+		int y = x + 1;
+
+		while (y < 10)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			putchar(',');
+			putchar(' ');
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
