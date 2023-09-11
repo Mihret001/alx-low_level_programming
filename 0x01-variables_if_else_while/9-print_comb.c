03ca1214c791a9f12625a5de84674a0ecd7e7b35
Mihret001 <Bizumercy@gmail.com>
2023-09-11 06:53:04 -0700

    added 9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,16 @@ int main(void)
 	int y;
 
 	for (x = 0; x <= 9; x++)
-		for (y = 0; y <=9; y++)
 		{
 		putchar(x + '0');
-		putchar(y + '0');
-		putchar(',');
-		putchar(' ');
+			{
+			if (x < 9)
+			putchar(',');
+			putchar(' ');
+			}
 		}
-
+	
 	putchar('\n');
-
+	
 	return (0);
 }
