849d51a10432a01b0b1f1325f16dbaaf6c593560
Mihret001 <Bizumercy@gmail.com>
2023-09-11 06:24:14 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+		for (y = 0; y <=9; y++)
+		{
+		putchar(x + '0');
+		putchar(y + '0');
+		putchar(',');
+		putchar(' ');
+		}
+
+	putchar('\n');
+
+	return (0);
+}
