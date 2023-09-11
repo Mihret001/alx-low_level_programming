71f43e9ca6da945af37b54a4563814b5dab9f309
Mihret001 <Bizumercy@gmail.com>
2023-09-11 09:59:16 -0700

    added 101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,31 +10,31 @@ int main(void)
 {
 	int x = 0;
 
-	 while (x < 8)
-	 {
-		 int y = x + 1;
+	while (x < 8)
+	{
+		int y = x + 1;
+		 
+		while (y < 9)
+		{
+			int z = y + 1;
 
-		 while (y < 9)
-		 {
-			 int z = y + 1;
+			while (z < 10)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (x != 7 || y != 8 || z != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
 
-			 while (z < 10)
-			 {
-				 putchar(x + '0');
-				 putchar(y + '0');
-				 putchar(z + '0');
-				 if (x != 7 || y != 8 || z != 9)
-				 {
-					 putchar(',');
-					 putchar(' ');
-				 }
-				 z++;
-			 }
-			 y++;
-		 }
-		 x++;
-	 }
-	 putchar('\n');
-
-	 return(0);
+	return (0);
 }
