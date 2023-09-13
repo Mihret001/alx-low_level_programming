1b790bb6d20d9f98aec7fba3e5ee08301006268a
Mihret001 <Bizumercy@gmail.com>
2023-09-13 12:57:53 -0700

    added 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ * 
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(lette);
+		letter++;
+	}
+	_putchar('\n');
+}
