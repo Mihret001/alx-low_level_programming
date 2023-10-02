2cc9c014378a6e4815f10a89ce255f50d035fb87
Mihret001 <Bizumercy@gmail.com>
2023-10-02 15:36:11 -0700

    added 1-args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the name of the program even if it changes
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: array of strings/arguments
  * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
