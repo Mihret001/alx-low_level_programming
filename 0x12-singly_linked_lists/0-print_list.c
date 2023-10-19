b2092aaf30cf400db1bb88222a7a2d81498d5c35
Mihret001 <Bizumercy@gmail.com>
2023-10-19 10:35:58 -0700

    added 0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a file called list_t into a list
+ * @h: a pointer to the file
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int x;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
