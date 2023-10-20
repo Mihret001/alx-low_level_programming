2389dcd2027a0d955f8f4878551a2926c25bb752
Mihret001 <Bizumercy@gmail.com>
2023-10-19 17:28:34 -0700

    added 3-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *x, *y;
 	size_t z;
 
-	new = malloc(sizeof(list_t));
+	x = malloc(sizeof(list_t));
 	if (x == NULL)
 		return (NULL);
 
