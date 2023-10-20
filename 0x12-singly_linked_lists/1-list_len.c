71257decf1a9db436ee089b24a8dc542cc2a70be
Mihret001 <Bizumercy@gmail.com>
2023-10-19 17:01:20 -0700

    added 1-list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- * Return: number of elements in h
+ * list_len - returns the number of elements in file list_t
+ * @h: pointer to the file
+ * Return: number of nodes
  */
 
 size_t list_len(const list_t *h)
