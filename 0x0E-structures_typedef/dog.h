95b3011d16ac76092fb67b15dad44a991fb5830f
Mihret001 <Bizumercy@gmail.com>
2023-10-23 11:41:29 -0700

    added files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog qualities
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog.
+ * Description: stores all information of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
