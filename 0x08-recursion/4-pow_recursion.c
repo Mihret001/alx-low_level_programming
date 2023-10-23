f5a09529c20f0050850938a402178e2698e87369
Mihret001 <Bizumercy@gmail.com>
2023-10-23 09:51:33 -0700

    added 4-pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "notrebloh.h"
+
+/**
+ * _pow_recursion - gives the value of x to the power of y
+ * @x: integer one
+ * @y: integer two
+ * Return: result of _ppow_recursion
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * (_pow_recursion(x, y - 1)));
+}
